e188e162fd2fa3d313b03c549e0a0a5f67d4b244
Александр Михайлов <thekulc@gmail.com>
2021-03-03 21:07:43 +0400

    feat: implement 3 fns: `max`, `min`, and `avg`

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || !array.length) return 0;
+  return array.reduce((a, c) => c < a ? c = c : c = a)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || !array.length) return 0;
+  return array.reduce((a, c) => c < a ? c = a : c = c)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || !array.length) return 0;
+  return array.reduce((a, c) => a += c) / array.length;
 }
